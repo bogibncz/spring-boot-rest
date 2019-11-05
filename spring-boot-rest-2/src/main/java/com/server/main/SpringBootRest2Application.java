28c94f5c28bfdee9d399bbcd0a27a0bbc616a282
bogibncz <bnczboglarka@gmail.com>
2019-11-05 22:21:01 +0100

    First commit

---
+++ spring-boot-rest-2/src/main/java/com/server/main/SpringBootRest2Application.java
@@ -0,0 +1,13 @@
+package com.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.bhaiti"})
+public class SpringBootRest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest2Application.class, args);
+	}
+
+}
